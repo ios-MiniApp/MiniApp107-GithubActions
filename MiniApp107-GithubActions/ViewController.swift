87a628985d567125f3b73daa22849adc4f6de924
Kota <maedakoutaa@gmail.com>
2022-10-03 13:57:37 +0900

    Initial Commit

---
+++ MiniApp107-GithubActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp107-GithubActions
+//
+//  Created by 前田航汰 on 2022/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
